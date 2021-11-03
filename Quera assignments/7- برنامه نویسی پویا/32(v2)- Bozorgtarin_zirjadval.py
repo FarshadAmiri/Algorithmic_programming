2e45b7f6c7e096dbdb1096dc48942dd91d33d3c7
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2021-11-03 10:13:15 +0330

    Changes to Quera 32 Assignment

--- Quera assignments/7- برنامه نویسی پویا/32(v2)- Bozorgtarin_zirjadval.py
+++ Quera assignments/7- برنامه نویسی پویا/32(v2)- Bozorgtarin_zirjadval.py
@@ -10,24 +10,23 @@ for i in range(m):
 b = dict()
 for i in range(n):
     for j in range(m):
-        for k in range(-1,m):
+        for k in range(m):
             b[(i,j,k)] = 0
 
 for i in range(n):
     for j in range(m):
         b[(i, j, j)] = a[j][i]
-        for k in range(j,m):
+        for k in range(j+1,m):
             b[(i,j,k)] = b[(i,j, k-1)] + a[k][i]
 
 
-answer = 10e-50
+max_sum = dict()
 
 for r1 in range(m):
     for r2 in range(r1,m):
-        max_sum = dict()
-        max_sum[0] = b[(0,r1,r2)]
+        max_sum[0,r1,r2] = b[0,r1,r2]
         for c2 in range(1,n):
-            max_sum[c2] = max(b[(c2,r1,r2)], max_sum[c2-1] + b[(c2,r1,r2)])
-        answer = max(max(max_sum.values()), answer)
+            max_sum[c2,r1,r2] = max(b[c2,r1,r2], max_sum[c2-1,r1,r2] + b[c2,r1,r2])
 
-print(int(answer))
+
+print(int(max(max_sum.values())))
