72b4b0c0f106eff3fbbee687f6d635fcd885a7d2
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2021-11-19 11:23:47 +0330

    Q37 updated

--- Quera assignments/7- برنامه نویسی پویا/37- Tafahom_(LCS).py
+++ Quera assignments/7- برنامه نویسی پویا/37- Tafahom_(LCS).py
@@ -1,24 +1,21 @@
-a = input()
-b = input()
+s = input()
+p = input()
 
+dp = [[0 for jj in range(len(p)+1)] for ii in range(len(s)+1)]
+seq = [['' for jjj in range(len(p)+1)] for iii in range(len(s)+1)]
 
-def subsets(word):
-    subs = set()
-    for mask in range(1<<len(word)):
-        x = ''
-        for i in range(len(word)):
-            if mask & 1<<i:
-                    x += word[i]
-        subs.add(x)
-    return subs
+for i in range(len(s)+1):
+    for j in range(len(p)+1):
+        if i == 0 or j == 0:
+            dp[i][j] = 0
+        elif s[i-1] == p[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+            seq[i][j] = seq[i-1][j-1] + s[i-1]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            seq[i][j] = max(seq[i][j - 1], seq[i - 1][j], key = len)
 
-
-a_sub = subsets(a)
-b_sub = subsets(b)
-
-intersects = a_sub.intersection(b_sub)
-
-lcs = max(intersects, key=len)
-
-print(len(lcs))
-print(lcs)
+# print(dp)
+# print(seq)
+print(dp[len(s)][len(p)])
+print(seq[len(s)][len(p)])
