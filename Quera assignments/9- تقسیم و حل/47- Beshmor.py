dbcd6a283eff5de2d30ba425f4feb269d4268d4d
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-06-04 08:44:12 +0430

    47- Beshmor modified

--- Quera assignments/9- تقسیم و حل/47- Beshmor.py
+++ Quera assignments/9- تقسیم و حل/47- Beshmor.py
@@ -2,8 +2,22 @@ t = int(input())
 series = []
 
 for i in range(t):
-    _ , k = map(int, input())
-    series.append(list(map(int, input())), k)
+    _ , k = map(int, input().split())
+    series.append((list(map(int ,input().split())), k))
 
-print(series)
-print()
+# print(series)
+
+def solve(serie, k):
+    ps = [(0, serie[0])]
+    for i in range(len(serie)-1):
+        ps.append((i + 1 , ps[-1][1] + serie[i + 1]))
+    # return ps
+    sorted_ps = sorted(ps, key = lambda x: x[1])
+    return sorted_ps
+
+
+
+
+
+for serie, k in series:
+    print(solve(serie, k))
