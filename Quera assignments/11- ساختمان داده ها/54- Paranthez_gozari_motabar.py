958683a34be85d3216c8b1cbaece43569e1b5f00
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-06-08 17:04:56 +0430

    54- Paranthez_gozari_moratab modified

--- Quera assignments/11- ساختمان داده ها/54- Paranthez_gozari_motabar.py
+++ Quera assignments/11- ساختمان داده ها/54- Paranthez_gozari_motabar.py
@@ -1,38 +1,31 @@
 string = input()
 
-# string1 = '(()(()))'
-# string2 = ')(()(()))('
-
-
 
 def solve(string):
-    stack = [(string[0],1)]
+    stack = []
     pairs = []
-    for index, i in enumerate(string[1:]):
-        if stack[-1][0] == '(':
-            if i == ')':
-                try:
-                    pairs.append((stack[-1][1], index+2))
-                    stack.pop()
-                except:
-                    pass
+    for id, char in enumerate(string):
+        if char == '(':
+            stack.append(('(', id + 1))
+        else:
+            if len(stack) == 0:
+                return -1
             else:
-                stack.append((i, index+2))
-        elif stack[-1][0] == ')':
-            stack.append((i, index+2))
-    if stack == []:
+                if stack[-1][0] == '(':
+                    pairs.append((stack[-1][1], id + 1))
+                    stack.pop()
+    if len(stack) == 0:
         return pairs
     else:
         return -1
 
 
-# print(solve(string1))
-# print(solve(string2))
+def print_answer(ans):
+    if ans == -1:
+        print(-1)
+    else:
+        for i, j in ans:
+            print(f'{i} {j}')
 
 
-ans = solve(string)
-if ans == -1:
-    print(-1)
-else:
-    for i,j in ans:
-        print(f'{i} {j}')
+print_answer(solve(string))
