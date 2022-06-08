ff30d94b8e1c418cd231ce9ba877c16c69e1b9cf
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-06-08 20:07:13 +0430

    55- History Added

--- Quera assignments/11- ساختمان داده ها/55- History.py
+++ Quera assignments/11- ساختمان داده ها/55- History.py
@@ -1,50 +1,45 @@
 q = int(input())
-history = []
-string = ''
+history = ['']
 
-def insert_it(char, loc):
-    global history, string
+def insert_it(query):
+    global history
+
+    string = history[-1]
+    loc = int(query.split()[1])
+    char = query.split()[2]
 
     string = string[ : loc - 1] + char + string[loc - 1 : ]
     history.append(string)
 
 
-def delete_it(loc):
-    global history, string
+def delete_it(query):
+    global history
 
+    string = history[-1]
+    loc = int(query.split()[1])
     string = string[ : loc - 1] + string[loc : ]
     history.append(string)
 
 
 def undo_it():
-    global history, string
+    global history
 
     history.pop()
-    string = history[-1]
 
 
 def do_query(query):
-    query_entities = query.split()
-    query_type = query_entities[0]
-    if len(query_entities) == 1:
-        q_loc = query_entities[1]
-    elif len(query_entities) == 2:
-        q_what = query_entities[2]
+    query_type = query.split()[0]
 
     if query_type == 'undo':
         undo_it()
-    else:
-        q_loc = int(query.split()[1])
-        q_what = query.split()[2]
-        if query_type == 'insert':
-            insert_it(q_what, q_loc)
-        elif query_type == 'delete':
-            delete_it(q_loc)
-
+    elif query_type == 'insert':
+        insert_it(query)
+    elif query_type == 'delete':
+        delete_it(query)
 
 
 for i in range(q):
     query = input()
     do_query(query)
 
-print(string)
+print(history[-1])
