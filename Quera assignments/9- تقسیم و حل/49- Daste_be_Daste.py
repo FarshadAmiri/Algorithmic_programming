a59111b3518286ce113e3c5633f786a310fcddf6
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-06-10 11:10:08 +0430

    49- Daste_be_Daste Modified

--- Quera assignments/9- تقسیم و حل/49- Daste_be_Daste.py
+++ Quera assignments/9- تقسیم و حل/49- Daste_be_Daste.py
@@ -1,26 +1,28 @@
 n = int(input())
 people = list(map(int, input().split()))
 
+ans = [0 for i in range(2**n)]
 
-ans = 0
-def solve(people):
+
+def solve(people, l, r):
     global ans
 
-    if len(people) == 1:
-        ans+= people[0]
+    if r - l == 1:
+        ans[l] += people[l]
         return
 
-    mid = int(len(people) / 2)
-    g1 = people[:mid]
-    g2 = people[mid: ]
+    mid = int((r + l) / 2)
+    # if r - l > 3:
+    max_l_mid = max(people[l : mid])
+    max_mid_r = max(people[mid : r])
 
-    if sum(g1) >= sum(g2):
-        #g2 should ne eliminated
-        ans += max(g2)
-        solve(g1)
-    else:
-        ans += max(g1)
-        solve(g2)
+    for i in range(l, mid):
+        ans[i] += max_mid_r
+    solve(people, l, mid)
+    for i in range(mid, r):
+        ans[i] += max_l_mid
+    solve(people, mid, r)
 
-solve(people)
-print(ans)
+
+solve(people, 0, len(people))
+print(max(ans))
