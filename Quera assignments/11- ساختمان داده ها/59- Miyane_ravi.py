183a86ec5d040e49735827482603e8fc4e0e5831
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-06-25 19:05:20 +0430

    59- Miyane_ravi Modified

--- Quera assignments/11- ساختمان داده ها/59- Miyane_ravi.py
+++ Quera assignments/11- ساختمان داده ها/59- Miyane_ravi.py
@@ -1,10 +1,12 @@
-import heapq
-numbers = [int(input()) for i in range(int(input()))]
-
-# print(numbers)
+import heapq, math, random
+# numbers = [int(input()) for i in range(int(input()))]
+# numbers = [3,9,1,4,12,15,8,7,12,11,1,6,11,8,4]
+numbers = [random.randint(0,100) for i in range(20)]
+print(numbers)
 
 h1 = [numbers[0]]
 h2 = []
+print(h1[0])
 
 if numbers[1] >= h1[0]:
     h2.append(numbers[1])
@@ -12,11 +14,23 @@ else:
     h1 = [numbers[1]]
     h2 = [numbers[0]]
 
+print(h1[0], h1[0] == h1[0])
 
-for i in numbers[1:]:
-    heapq.heapify(h1)
+for id,x in enumerate(numbers[2:]):
+    heapq._heapify_max(h1)
     heapq.heapify(h2)
+
+    if x > h1[0]:
+        heapq.heappush(h2, x)
+        if len(h2) > len(h1):
+            h1.append(heapq.heappop(h2))
+    else:
+        heapq.heappush(h1, x)
+        if len(h1) > len(h2) + 1:
+            heapq.heappush(h2, heapq._heappop_max(h1))
+
     heapq._heapify_max(h1)
 
-
+    real_median = sorted(numbers[:id+3])[math.ceil(((id+3)/2)-1)]
+    print(f'{x} Added --> {h1[0]}  real_median = {real_median} {real_median == h1[0]}')
 
--- Quera assignments/draft_2.py
+++ Quera assignments/draft_2.py
@@ -1,13 +1,2 @@
-import heapq
-
-h = [8,4,6,9,11,4,3,15,2]
-
-heapq.heapify(h)
-print(h)
-print(h[0])
-print(heapq.heappop(h))
-
-heapq._heapify_max(h)
-print(h)
-print(h[0])
-print(heapq._heappop_max(h))
+import statistics
+print(statistics.median([1,3,4,9]))
