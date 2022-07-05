39e41cb2316a35aa7003a4e41f961a68d1f4522a
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-07-05 17:55:44 +0430

    60- Barname_riziye_foroudgah Added

--- Quera assignments/draft_2.py
+++ Quera assignments/draft_2.py
@@ -1,2 +1,7 @@
-import statistics
-print(statistics.median([1,3,4,9]))
+x = [1,5,4,7,9,3,4,6,5,2]
+
+for i in x:
+    for j in range(i - 2, i + 3):
+        if j == 6:
+            break
+        print(j)
