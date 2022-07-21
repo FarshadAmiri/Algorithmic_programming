1593455de212be9e4f97f9549bb8a4f7764e434a
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-07-21 17:19:52 +0430

    67- Kadoye_mashti Modified

--- Quera assignments/12- پیش پردازش و بهینه سازی/67- Kadoye_mashti.py
+++ Quera assignments/12- پیش پردازش و بهینه سازی/67- Kadoye_mashti.py
@@ -1,8 +1,6 @@
-# n,k = map(int, input().split())
-# requests = [input().split() for i in range(n)]
-# requests = [(int(j),int(k)) for j,k in requests]
-n , k = 3 , 2
-requests = [(1, 3), (2, 3), (4, 4)]
+n,k = map(int, input().split())
+requests = [input().split() for i in range(n)]
+requests = [(int(j),int(k)) for j,k in requests]
 
 max_b = max([j for i,j in requests])
 
@@ -10,17 +8,14 @@ dp = [0] * (max_b + 1)
 dp[0] = 1
 
 for i in range(1,max_b+1):
-    dp[i] += dp[i-1]
+    dp[i] += (dp[i-1]) % 1000000007
     if i >= k:
-        dp[i] += dp[i - k]
+        dp[i] += (dp[i - k]) % 1000000007
 
-# print(dp)
 ps = [0] * (max_b + 1)
 ps[0] = dp[0]
 for i in range(1,max_b+1):
-    ps[i] = ps[i-1] + dp[i]
-
-# print(ps)
+    ps[i] = (ps[i-1] + dp[i]) % 1000000007
 
 for i,j in requests:
     print((ps[j] - ps[i-1]) % 1000000007)
