30810e0fa5b44683498d6224d4d82549fc5ce48c
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-07-23 21:58:29 +0430

    70- Bishine_dar_araye_moteghayyer (Made another Progress)

--- Quera assignments/12- پیش پردازش و بهینه سازی/70- Bishine_dar_araye_moteghayyer.py
+++ Quera assignments/12- پیش پردازش و بهینه سازی/70- Bishine_dar_araye_moteghayyer.py
@@ -1,8 +1,8 @@
 import math
-n , q = map(int, input().split())
-series = list(map(int, input().split()))
-queries = [list(map(int, input().split())) for i in range(q)]
-# n, q, series, queries = 5, 6, [1, 2, 3, 4, 5], [[1, 1, 3], [2, 1, 5], [1, 1, 3], [1, 0, 4], [2, 3, 7], [1, 0, 3]]
+# n , q = map(int, input().split())
+# series = list(map(int, input().split()))
+# queries = [list(map(int, input().split())) for i in range(q)]
+n, q, series, queries = 5, 6, [1, 2, 3, 4, 5], [[1, 1, 3], [2, 1, 5], [1, 1, 3], [1, 0, 4], [2, 3, 7], [1, 0, 3]]
 
 r = math.floor(math.sqrt(n))
 nr = math.ceil(math.sqrt(n))
@@ -20,37 +20,27 @@ def calculate_max_in_range(l,r):
     global ranges, max_in_ranges
     for index,rr in enumerate(ranges):
         if rr[0] == l:
-            idx = index
+            max_in_ranges[index] = max(series[l:r+1])
             break
-    max_in_ranges[idx] = max(series[l:r+1])
 
 for i,j in ranges:
     calculate_max_in_range(i,j)
 
-# print(series, ranges, max_in_ranges, sep='\n')
+# print(f' series = {series} \n ranges = {ranges} \n max_in_ranges = {max_in_ranges}')
 
 def calculate_arbitrary_max(l,r):
     global ranges, max_in_ranges, series
-    sub_ranges_idx = []
-    out_data = []
+    sub_ranges_maxes = []
     for idx,rr in enumerate(ranges):
         if (rr[0] >= l) and (rr[1] <= r):
-            sub_ranges_idx.append(idx)
+            sub_ranges_maxes.append(max_in_ranges[idx])
         elif (rr[0] < l) and (rr[1] >= l):
             for x in range(l, rr[1] + 1):
-                out_data.append(series[x])
+                sub_ranges_maxes.append(series[x])
         elif (rr[0] <= r) and (rr[1] > r):
             for x in range(rr[0], r + 1):
-                out_data.append(series[x])
-    # print('out_data = ',out_data)
-    # print('sub_ranges_idx = ', sub_ranges_idx)
-    # print('max_in_ranges', max_in_ranges)
-    maxes = []
-    for ii in sub_ranges_idx:
-        maxes.append(max_in_ranges[ii])
-    for ii in out_data:
-        maxes.append(ii)
-    return max(maxes)
+                sub_ranges_maxes.append(series[x])
+    return max(sub_ranges_maxes)
 
 
 
---
+++ Quera assignments/draft_2.py
@@ -0,0 +1,1 @@
+print(range(5,9).range(5,7))
