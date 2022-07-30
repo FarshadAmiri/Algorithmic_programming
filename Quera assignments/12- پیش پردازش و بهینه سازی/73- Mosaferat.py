2fa805f66761790159433ee4ec8cf143de7eab69
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-07-30 04:35:07 +0430

    73- Mosaferat Modified

--- Quera assignments/12- پیش پردازش و بهینه سازی/73- Mosaferat.py
+++ Quera assignments/12- پیش پردازش و بهینه سازی/73- Mosaferat.py
@@ -1,9 +1,9 @@
 import sys
 sys.setrecursionlimit(10**5 + 100)
 
-# n = int(input())
-# edges = [tuple(map(int, input().split())) for i in range(n)]
-n, edges = 4, [(1, 1), (3, 1), (3, 3), (1, 3)]
+n = int(input())
+edges = [tuple(map(int, input().split())) for i in range(n)]
+# n, edges = 4, [(1, 1), (3, 1), (3, 3), (1, 3)]
 # n, edges = 4, [(1, 1), (2, 2), (3, 3), (3, 4)]
 
 vertices  = set()
@@ -16,4 +16,31 @@ for i in range(n):
         if (e1[0] == e2[0]) or (e1[1] == e2[1]):
             vertices.add((min(i,j), max(i,j)))
 
-print(vertices)
+# print(vertices)
+
+adj = [[] for i in range(n)]
+mark = [False] * (n)
+components = list()
+connected_components = list()
+
+for i,j in vertices:
+    adj[i].append(j)
+    adj[j].append(i)
+
+
+def DFS(v):
+    mark[v] = True
+    components.append(v)
+    for u in adj[v]:
+        if not mark[u]:
+            DFS(u)
+
+
+for i in range(n):
+    if mark[i]:
+        continue
+    DFS(i)
+    connected_components.append(components)
+    components = []
+
+print(len(connected_components) - 1)
